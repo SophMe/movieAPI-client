6e6bd071cc716f3963f2fb91d9b8eb35f331f743
SophMe <116891325+SophMe@users.noreply.github.com>
2023-01-25 16:11:38 +0100

    add useEffect and empty array

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27132,65 +27132,30 @@ var _movieView = require("../movie-view/movie-view");
 var _s = $RefreshSig$();
 const MainView = ()=>{
     _s();
-    const [movies, setMovies] = (0, _react.useState)([
-        {
-            "id": 1,
-            "Title": "Amercian Beauty",
-            "Description": "Lester Burnham is a gainfully employed suburban husband and father. Fed up with his boring, stagnant existence, he quits his job and decides to reinvent himself as a pot-smoking, responsibility-shirking teenager. What follows is at once cynical, hysterical, and, eventually, tragically uplifting.",
-            "Year": 1999,
-            "Director": {
-                "Name": "Sam Mendes",
-                "Birth": 1965
-            },
-            "ImagePath": "https://www.imdb.com/title/tt0169547/mediaviewer/rm2430294272/?ref_=tt_ov_i",
-            "Main": "Kevin Spacey",
-            "Genre": {
-                "Name": "Drama",
-                "Description": "Drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-            }
-        },
-        {
-            "id": 2,
-            "Title": "The Shawshenk Redemption",
-            "Description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-            "Year": 1994,
-            "Director": {
-                "Name": "Frank Darabont",
-                "Birth": 1959
-            },
-            "ImagePath": "https://www.imdb.com/Title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i",
-            "Main": "Tim Robbins",
-            "Genre": {
-                "Name": "Drama",
-                "Description": "Drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-            }
-        },
-        {
-            "id": 3,
-            "Title": "Braveheart",
-            "Description": "Scottish warrior William Wallace leads his countrymen in a rebellion to free his homeland from the tyranny of King Edward I of England.",
-            "Year": 1995,
-            "Director": {
-                "Name": "Mel Gibson",
-                "Birth": 1956,
-                "Bio": "After college, Mel had a few stints on stage and starred in a few TV shows. Eventually, he was chosen to star in the films Mad Max (1979) and Tim - Kann das Liebe sein? (1979), co-starring Piper Laurie. The small budgeted Mad Max made him known worldwide, while Tim garnered him an award for Best Actor from the Australian Film Institute (equivalent to the Oscar)."
-            },
-            "ImagePath": "https://www.imdb.com/Title/tt0112573/mediaviewer/rm3170786816/?ref_=tt_ov_i",
-            "Main": "Mel Gibson",
-            "Genre": {
-                "Name": "Historical drama",
-                "Description": "A historical drama is a work set in a past time period, usually used in the context of film and television."
-            }
-        }
-    ]);
+    const [movies, setMovies] = (0, _react.useState)([]); // empty array to be fed by API
     // initial state: no movie card is clicked 
     const [selectedMovie, setSelectedMovie] = (0, _react.useState)(null);
+    (0, _react.useEffect)(()=>{
+        //fetch("https://90s-movie-api-liart.vercel.app/movies")
+        fetch("http://localhost:8080/movies").then((response)=>response.json()).then((data)=>{
+            console.log("movies from api", data);
+            const moviesFromApi = data.map((doc)=>{
+                return {
+                    Title: doc.Title,
+                    Year: doc.Year,
+                    Director: doc.Director,
+                    Description: doc.Description
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
     if (selectedMovie) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieView.MovieView), {
         movie: selectedMovie,
         onBackClick: ()=>setSelectedMovie(null)
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 62,
+        lineNumber: 31,
         columnNumber: 7
     }, undefined) // set state to null when closing MovieView
     ;
@@ -27198,7 +27163,7 @@ const MainView = ()=>{
         children: "The list is empty!"
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 67,
+        lineNumber: 36,
         columnNumber: 12
     }, undefined);
     else return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -27209,16 +27174,16 @@ const MainView = ()=>{
                 }
             }, movie.id, false, {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 73,
+                lineNumber: 42,
                 columnNumber: 11
             }, undefined))
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 71,
+        lineNumber: 40,
         columnNumber: 7
     }, undefined);
 };
-_s(MainView, "UCXJWjBs9wC+ws/vssh1SfolLjg=");
+_s(MainView, "PO+XgOji7E32nFJj3H5UPLPJ7w4=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,62 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      "id" : 1,
-      "Title": "Amercian Beauty",
-      "Description": "Lester Burnham is a gainfully employed suburban husband and father. Fed up with his boring, stagnant existence, he quits his job and decides to reinvent himself as a pot-smoking, responsibility-shirking teenager. What follows is at once cynical, hysterical, and, eventually, tragically uplifting.",
-      "Year": 1999,
-      "Director": {
-        "Name": "Sam Mendes",
-        "Birth": 1965
-      },
-      "ImagePath": "https://www.imdb.com/title/tt0169547/mediaviewer/rm2430294272/?ref_=tt_ov_i",
-      "Main": "Kevin Spacey",
-      "Genre": {
-        "Name": "Drama",
-        "Description": "Drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-      }
-    },
-    {
-      "id" : 2,
-      "Title": "The Shawshenk Redemption",
-      "Description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      "Year": 1994,
-      "Director": {
-        "Name": "Frank Darabont",
-        "Birth": 1959
-      },
-      "ImagePath": "https://www.imdb.com/Title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i",
-      "Main": "Tim Robbins",
-      "Genre": {
-        "Name": "Drama",
-        "Description": "Drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-      }
-    },
-    {
-      "id" : 3,
-      "Title": "Braveheart",
-      "Description": "Scottish warrior William Wallace leads his countrymen in a rebellion to free his homeland from the tyranny of King Edward I of England.",
-      "Year": 1995,
-      "Director": {
-          "Name": "Mel Gibson",
-          "Birth": 1956,
-          "Bio": "After college, Mel had a few stints on stage and starred in a few TV shows. Eventually, he was chosen to star in the films Mad Max (1979) and Tim - Kann das Liebe sein? (1979), co-starring Piper Laurie. The small budgeted Mad Max made him known worldwide, while Tim garnered him an award for Best Actor from the Australian Film Institute (equivalent to the Oscar)."
-      },
-      "ImagePath":"https://www.imdb.com/Title/tt0112573/mediaviewer/rm3170786816/?ref_=tt_ov_i",
-      "Main": "Mel Gibson",
-      "Genre": {
-          "Name": "Historical drama",
-          "Description": "A historical drama is a work set in a past time period, usually used in the context of film and television."
-      }
-    }
-  ]);
+  const [movies, setMovies] = useState([]); // empty array to be fed by API
 // initial state: no movie card is clicked 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  useEffect(() => {
+    //fetch("https://90s-movie-api-liart.vercel.app/movies")
+    fetch("http://localhost:8080/movies")
+    .then((response) => response.json())
+    .then((data) => {
+    console.log("movies from api", data);
+      const moviesFromApi = data.map((doc) => {
+        return {
+          Title: doc.Title,
+          Year: doc.Year,
+          Director: doc.Director,
+          Description: doc.Description
+        };
+      });
+      setMovies(moviesFromApi);
+    });
+  }
+  , []);
+  
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} /> // set state to null when closing MovieView
